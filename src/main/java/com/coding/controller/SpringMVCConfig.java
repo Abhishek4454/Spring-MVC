656acba29735d8f55600f09270ebf9431f9a16fc
abhishek <abhishek.kumar7152@gmail.com>
2023-02-13 01:34:11 +0530

    Annotation based Spring MVC.Removed the configurations

---
+++ src/main/java/com/coding/controller/SpringMVCConfig.java
@@ -0,0 +1,21 @@
+package com.coding.controller;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@ComponentScan({"com.coding.controller"})
+public class SpringMVCConfig {
+
+	@Bean
+	public InternalResourceViewResolver viewResolver() {
+		
+		InternalResourceViewResolver ivr= new InternalResourceViewResolver();
+		ivr.setPrefix("/WEB-INF/views/");
+		ivr.setSuffix(".jsp");
+		
+		return ivr;
+	}
+}
